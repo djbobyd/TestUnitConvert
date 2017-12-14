67a4cdb2beecc2b877cb68de519661966a3eeb43
Borislav Arnaudov <boby@boby.bg>
2017-12-14 08:26:37 +0200

    Add project files.

---
+++ TestUnitConvert/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsNet;
+
+namespace TestUnitConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = UnitConverter.ConvertByAbbreviation(5, "Length", "m", "cm");
+            QuantityType[] quantityTypes = Enum.GetValues(typeof(QuantityType)).Cast<QuantityType>().ToArray();
+
+            Mass.ParseUnit("kg");
+            Console.WriteLine(test);
+            Console.ReadKey();
+        }
+    }
+}
